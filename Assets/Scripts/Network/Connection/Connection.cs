c63ea36d89603a7a11e718808de98c6fdc216047
beepe0 <dshakhun@gmail.com>
2023-10-29 13:45:18 +0100

    Zaktualizowałem

---
+++ Assets/Scripts/Network/Connection/Connection.cs
@@ -0,0 +1,19 @@
+﻿using Network.UServer;
+using Singleton;
+using UnityEngine;
+
+namespace Network.Connection
+{
+    public class Connection : Singleton<Connection>
+    {
+        private void Awake()
+        {
+            UNetworkCore.GeneralRules = new ConnectionRules.GeneralRules();
+            UNetworkCore.InputRules = new ConnectionRules.InputRules();
+            UNetworkCore.OutputRules = new ConnectionRules.OutputRules();
+            
+            UNetworkCore.RulesHandler.AddNewRule((ushort)ConnectionRules.PacketType.OnWelcome, UNetworkCore.InputRules.OnWelcome); 
+            UNetworkCore.RulesHandler.AddNewRule((ushort)ConnectionRules.PacketType.SynchronizePosition, ((ConnectionRules.InputRules)UNetworkCore.InputRules).SynchronizePosition); 
+        }
+    }
+}
---
+++ Assets/Scripts/Network/Connection/ConnectionRules.cs
@@ -0,0 +1,52 @@
+﻿using Network.UNTools;
+using Network.UServer;
+using UnityEngine;
+
+namespace Network.Connection
+{
+    public class ConnectionRules
+    {
+        public enum PacketType : ushort
+        {
+            OnWelcome,
+            SynchronizePosition
+        }
+        public class GeneralRules : UNetworkIORules.IGeneralRules
+        {
+            public void OnDisconnect(ushort clientId)
+            {
+                Debug.Log($"[ID: {clientId}] The client disconnected!");
+            }
+
+            public void OnClose()
+            {
+                Debug.Log($"The server closed!");
+            }
+        }
+        public class InputRules : UNetworkIORules.IInputRules
+        {
+            public void OnWelcome(ushort clientId, UNetworkReadablePacket inputPacket)
+            {
+                Debug.Log($"ID: {inputPacket.Index}, ID: {clientId} LN: {inputPacket.Length}, PT: {(PacketType)inputPacket.PacketNumber}, DT: {inputPacket.ReadString()}");
+            }
+            
+            public void SynchronizePosition(ushort clientId, UNetworkReadablePacket inputPacket)
+            {
+                Vector3 pos = new Vector3(inputPacket.ReadFloat(), inputPacket.ReadFloat(), inputPacket.ReadFloat());
+                Debug.Log($"ID: {inputPacket.Index}, ID: {clientId} LN: {inputPacket.Length}, PT: {(PacketType)inputPacket.PacketNumber}, DT: {pos}");
+                Connection.Instance.gameObject.transform.position = pos;
+            }
+        }
+        public class OutputRules : UNetworkIORules.IOutputRules
+        {
+            public void OnWelcome(ushort clientId)
+            {
+                UNetworkIOPacket packet = new UNetworkIOPacket((ushort)PacketType.OnWelcome);
+                
+                packet.Write("123");
+                
+                UNetworkCore.DataHandler.Tcp.SendData(clientId, packet);
+            }
+        }
+    }
+}
